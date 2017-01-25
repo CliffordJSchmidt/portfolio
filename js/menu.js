4458d0f43b64b4f1221f85dea4d68f230e1c51fa
CliffordJSchmidt <clifford.j.schmidt@gmail.com>
2017-01-25 18:42:06 -0500

    1st direct upload of notes for greyscale portfolio
    grey scale web developer portfolio. newly added menu bar and background.

---
+++ js/menu.js
@@ -0,0 +1,11 @@
+/* nav bar from http://www.w3schools.com/howto/howto_js_topnav.asp */
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
