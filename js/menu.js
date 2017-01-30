746cb14465643e92de82a97f89a4704a85b2f21b
Unknown <clifford.j.schmidt@gmail.com>
2017-01-29 23:37:32 -0500

    updates on files
    needs email form, also photo gallery would be better with a slick carousel

--- js/menu.js
+++ js/menu.js
@@ -9,3 +9,19 @@ function myFunction() {
         x.className = "topnav";
     }
 }
+
+
+
+
+/* nav bar from http://www.w3schools.com/howto/howto_js_topnav.asp */
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("mybtmnav");
+    if (x.className === "btmnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "btmnav";
+    }
+}
+
