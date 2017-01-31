c157560d8c4a1f6c8e0aa4c146340a42e259da37
Unknown <clifford.j.schmidt@gmail.com>
2017-01-31 09:55:40 -0500

    update

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
 <?php include "../pages/landingpage.html";?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Portfolio | Clifford John Schmidt</title>
+    <meta http-equiv="refresh" content="=0;URL=http://cjs.data4you.cz/portfolio/landingpage/" />
+</head>
+<body>
+    
+</body>
+</html>
